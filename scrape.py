2c626396d73b9c8757aad3110290da6a3c1c132e
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2022-06-23 20:50:56 -0500

    Scraped category, date, publication, blurb and links within blurb for most categories
    For categories without bullet points, this works, so far, without fail. For those with bullet points, there are still a few bugs due to inconsistencies within the website

--- scrape.py
+++ scrape.py
@@ -1,14 +1,175 @@
 from bs4 import BeautifulSoup  #Importing the Beautiful Soup Library
 import requests				   #Importing the requests library
 import time					   #Importing the time library
-import csv					   #Importing the csv module
+import csv
+import pandas as pd
 
+from zmq import curve_public					   #Importing the csv module
+
+def bulletPointScrape(arr, link, date, category):
+    print(arr)
+    curItem = {}
+    blurbText = ''
+    links = []
+
+    for childPos in range(len(arr)-1):
+        blurbText += arr[childPos].text
+        if arr[childPos].name == 'a':
+            links.append(arr[childPos].get('href'))
+    
+    blurbText = blurbText[1:]
+
+    curItem['category'] = category
+    curItem['date'] = date
+    curItem['publication'] = arr[-1].text.strip()[1:-1]
+    curItem['blurb'] = blurbText
+    curItem['links-within-blurb'] = links
+    curItem['article-link'] = link
+
+    digestItems.append(curItem)
+
+
+digestItems = []
+
+#digestLink is the link to an article
 def getDigestItems(digestLink):
+    print(digestLink)
+
     response = requests.get(digestLink)
     soup = BeautifulSoup(response.text, 'lxml')
+    curDigestLink = digestLink
+
+    date = ''
+
+    datePosted = soup.find(class_='posted-on')
+    dateTag = list(datePosted.children)[-1]
+    date = dateTag.text
+
+    #all of the content we want within each article (aka the paragraphs of information)
     entryContentEls = soup.find_all(class_='entry-content')
+
+
     for entry in entryContentEls:
-        print(entry.find_all('p'))
+
+        #each p tag is a category in each article
+        paragraphLinks = entry.find_all('p')
+
+        for p in paragraphLinks:
+            curItem = {}
+            # print(p.find('strong'))
+
+            #run though each category by finding the strong tags
+            strongTags = p.find_all('strong')
+            # for i in range(len(strongTags)-1,0,-1):
+            #     if str(type(strongTags[i])) == "<class 'bs4.element.Tag'>":
+            #                 if list(strongTags[i].children)[-1].name == 'br':
+            #                     print('del cause thing while')
+            #                     del strongTags[i]
+
+            strongTags.extend(p.find_all('b'))
+
+            #the fitst string tag should be the category name
+            if len(strongTags) < 1:
+                continue
+            curCategory = strongTags[0].text[:-1]
+            
+            #if there is more than the one strong tag, this means there are bullet points
+            if len(strongTags) > 1:
+                addingString = ''
+
+                #run through each of the bullet points, skip the first one because the first on is the category name
+                # for curStringIndex in range(1,len(strongTags)):
+                #     curItem = {}
+                #     curString = []
+                #     links = []
+                #     publication = 'Unknown'
+
+                #     addingString = strongTags[curStringIndex].next_sibling
+
+                #     #until there is a new line or no more elements, keep adding the next element in the tree
+
+
+                #     while addingString is not None and addingString.name != 'br' and addingString.name != 'b':
+                #         if str(type(addingString)) == "<class 'bs4.element.Tag'>":
+                #             if list(addingString.children)[-1].name == 'br':
+                #                 print('broke out cause thing while')
+                #                 break
+
+                #         curString.append(addingString)
+                #         addingString = addingString.next_sibling
+
+                #     blurbText = ''
+
+                #     #for each element, if it is an a tag, get the link and text, if it is a em tag, that means it is the publication, and if it is neither, it is just text
+                #     for tag in curString:
+                #         if tag.name == 'a':
+                #             links.append(tag.get('href'))
+                #             blurbText += tag.text
+                #         elif tag.name == 'em' or tag.name == 'i':
+                #             publication = tag.text.strip()[1:-1]
+                #         else:
+                #             blurbText += tag.text
+
+                #     curItem['category'] = curCategory
+                #     curItem['date'] = date
+                #     curItem['publication'] = publication
+                #     curItem['blurb'] = blurbText
+                #     curItem['links-within-blurb'] = links
+                #     curItem['article-link'] = digestLink
+
+                #     digestItems.append(curItem)
+
+                currentString = []
+                gettingDataActive = False
+
+                for child in p:
+                    print(child.text)
+                    if not gettingDataActive and len(child.text.strip()) > 0 and child.text.strip()[0] == '•':
+                        gettingDataActive = True
+                        currentString.append(child)
+                    elif len(child.text.strip()) > 0 and child.text.strip()[0] == '(':
+                        currentString.append(child)
+                        gettingDataActive = False
+                        bulletPointScrape(currentString, link=digestLink, date=date, category = curCategory)
+                        currentString = []
+                    elif len(child.text.strip()) > 0 and child.text.strip()[0] == '•':
+                        gettingDataActive = False
+                        bulletPointScrape(currentString, link=digestLink, date=date, category = curCategory)
+                    elif gettingDataActive:
+                        currentString.append(child)
+
+
+            #if there is only one strong tag, that means no bullet points
+            else:
+                curItem['category'] = curCategory
+                curItem['date'] = date
+
+                publication = 'Unknown'
+
+                if p.find('em') is not None:
+                    publication = p.find('em').text.strip()[1:-1]
+                elif p.find('i') is not None:
+                    publication = p.find('i').text.strip()[1:-1]
+
+                curItem['publication'] = publication
+
+
+                blurbText = ''
+                for element in p:
+                    if element.name != 'strong' and element.name != 'em' and element.name != 'i':
+                        blurbText += element.text
+
+                curItem['blurb'] = blurbText
+
+                links = []
+                for link in p.find_all('a'):
+                    links.append(link.get('href'))
+
+                curItem['links-within-blurb'] = links
+                curItem['article-link'] = digestLink
+
+                digestItems.append(curItem)
+
         #TODO should get Category, date, publication, blurb, link to the page, plus all links within the blurb, city, state
 
 baseUrl = 'https://energynews.us/category/digest/page/{0}/'
@@ -29,9 +190,6 @@ while i < 2:
             for el in link:
                 digestLinks.append(el.get('href'))
     i+=1
-    
-
-print(digestLinks)
 
 # Get all the links for all of the pages, then extract info we are looking for
 for link in digestLinks:
@@ -40,4 +198,6 @@ for link in digestLinks:
 # TODO store in a structured way.
 
 
-    
+# print(digestItems)
+df = pd.DataFrame.from_dict(digestItems)
+df.to_excel('digestItems.xlsx')
